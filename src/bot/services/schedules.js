ee6005d8696a3f5eef094f54e0bb4999ff466f2a
GiovaniZaparoli <giovani.zaparoli@outlook.com>
2019-11-13 20:44:45 -0300

    Escolher um serviço e persistir em memória

--- src/bot/services/schedules.js
+++ src/bot/services/schedules.js
@@ -1,3 +1,6 @@
+const axios = require('axios')
+const auth_token = 'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE1NjgxNTU0OTR9.oDgeY2Tbr6eeKStrHG2_t0Y7oCfl_6JBhidCBbn5qzs'
+
 exports.verifySchedules = (request) => {
   let schedules_regex = ['Horário', 'Hora', 'horario', 'hora', 'horários', 'horarios', 'horas', 'Horas',
   'Horários']
--- src/bot/services/services.js
+++ src/bot/services/services.js
@@ -1,12 +1,13 @@
 const axios = require('axios')
 const auth_token = 'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE1NjgxNTU0OTR9.oDgeY2Tbr6eeKStrHG2_t0Y7oCfl_6JBhidCBbn5qzs'
+const dirty = require('dirty')
+const db = dirty('user.db')
+
+const services_regex = ['SERVIÇO', 'SERVICO', 'SERVIÇOS', 'SERVICOS']
 
 exports.verifyServices = async (request) => {
-
-  let services_regex = ['serviços', 'Serviço', 'Serviços', 'servico', 'Servico',
-  'serviço']
   
-  var message = request.text
+  var message = request.text.toUpperCase()
 
   var contains_services = false
 
@@ -20,7 +21,7 @@ exports.verifyServices = async (request) => {
     
     services = await getServices().then(response => {
       response.forEach(function(service) {
-        services = services + '- ' + service.name + '\n'
+        services = services + service.id + '- ' + service.name + '\n'
       })
       return services
     })
@@ -29,6 +30,35 @@ exports.verifyServices = async (request) => {
   }
 
   return ''
+}
+
+exports.chooseService = async (request) => {
+  var message = request.text.toUpperCase()
+
+  var contains_choose = false
+  var contain_service = false
+  var selectedService = ''
+
+  var services = await getServices()
+
+  services.forEach((service) => {
+    if (contain_service === 1) return
+    if (message.endsWith(service.id.toString())) {
+      contain_service = 1
+      db.set('service', { selected: service.id } )
+      selectedService = `Você selecionou o serviço de Nº ${service.id} - ${service.name}`  
+    }
+  })
+
+  services_regex.forEach(function(word) {
+    if (contains_choose === 1) return
+    contains_choose = contains_choose + message.includes(word)
+  })
+
+  if (contains_choose === 1 && contain_service === 1) return selectedService
+
+  return ''
+
 }
 
 async function getServices () {
